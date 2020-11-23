45b4d287081f64e1e25b5c690341215b6c10d954
unknown <tweenlicious@gmail.com>
2020-11-23 08:41:33 +0700

    Added boss sequence, dungeon tile checking

--- dungeon.pl
+++ dungeon.pl
@@ -6,11 +6,78 @@
 :- dynamic(inDungeon/1).
 
 dungeon :-
+    start_flag(true),
+
     % Cek apakah player berada di posisi dungeon
     object(X, Y, 'P'),
     object(XDungeon, YDungeon, 'D'),
     X == XDungeon,
     Y == YDungeon,
 
-    retract(inDungeon(0)),
-    assertz(inDungeon(1)).
+    % Set location status
+    retract(inDungeon(_)),
+    assertz(inDungeon(1)),
+    
+    % Boss cinematic cutscene
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl,    
+    write('%                                            __----~~~~~~~~~~~------___        %'),nl,
+    write('%                                .  .   ~~//====......          __--~ ~~       %'),nl,
+    write('%                -.            \\_|//     |||\\\\  ~~~~~~::::... /~               %'),nl,
+    write('%            ___-==_       _-~o~  \\/    |||  \\\\            _/~~-               %'),nl,
+    write('%    __---~~~.==~||\\=_    -_--~/_-~|-   |\\\\   \\\\        _/~                    %'),nl,
+    write('%_-~~     .=~    |  \\\\-_    \'-~7  /-   /  ||    \\      /                       %'),nl,
+    write('%.~       .~       |   \\\\ -_    /  /-   /   ||      \\   /                      %'),nl,
+    write('%/  ____  /         |     \\\\ ~-_/  /|- _/   .||       \\ /                      %'),nl,
+    write('%|~~    ~~|--~~~~--_ \\     ~==-/   | \\~--===~~        .\\                       %'),nl,
+    write('%        \'         ~-|      /|    |-~\\~~       __--~~                          %'),nl,
+    write('%                    |-~~-_/ |    |   ~\\_   _-~            /\\                  %'),nl,
+    write('%                        /  \\     \\__   \\/~                \\__                 %'),nl,
+    write('%                    _--~ _/ | .-~~____--~-/                  ~~==.            %'),nl,
+    write('%                    ((->/~   \'.|||\' -_|    ~~-/ ,              . _||          %'),nl,
+    write('%                                -_     ~\\      ~~---l__i__i__i--~~_/          %'),nl,
+    write('%                                _-~-__   ~)  \\--______________--~~            %'),nl,
+    write('%                            //.-~~~-~_--~- |-------~~~~~~~~                   %'),nl,
+    write('%                                    //.-~~~--\\                                %'),nl,
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl, !,
+
+    % Initialize boss
+    EnemyID is 4,
+    retract(cenemyid(_)),
+    assertz(cenemyid(EnemyID)),
+
+    Level is 1,
+    retract(cenemylevel(_)),
+    assertz(cenemylevel(Level)),
+
+    enemy_hp(EnemyID, EnemyHP),
+    retract(cenemyhp(_)),
+    assertz(cenemyhp(EnemyHP)),
+
+    enemy_attack(EnemyID, EnemyAttack),
+    retract(cenemyattack(_)),
+    assertz(cenemyattack(EnemyAttack)),
+
+    enemy_defense(EnemyID, EnemyDefense),
+    retract(cenemydefense(_)),
+    assertz(cenemydefense(EnemyDefense)),
+
+    % Enter battle
+    retract(inBattle(_)),
+    assertz(inBattle(1)),
+    battle, !.
+
+dungeon :-
+    start_flag(false),
+    !,
+    write('Please start the game. Type \'start.\'').
+
+dungeon :-
+    write('Please go to the dungeon!'),nl.
+
+exitDungeon :- 
+    retract(inDungeon(1)),
+    assertz(inDungeon(0)),
+
+    retract(inBattle(_)),
+    assertz(inBattle(0)),
+    write('You wake up, all disoriented and hazy, at the entrance of the dungeon.'), nl.
